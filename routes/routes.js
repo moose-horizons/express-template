374232710c77634943b16e0196d16733fdf267ee
Mustafa Paksoy <mustafa@joinhorizons.com>
2017-07-18 09:51:31 -0700

    Update routes.js

--- routes/routes.js
+++ routes/routes.js
@@ -10,7 +10,7 @@ router.get('/', function(req, res, next) {
   res.render('home');
 });
 
-router.get('/moose', function(req, res) {
+router.get('/moose2', function(req, res) {
   res.send('THIS IS A NEW ROUTE');
 });
 
