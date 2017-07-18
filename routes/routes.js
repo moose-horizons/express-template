c4f06e779e3f354db10c78df3acdccbf21e5e1c6
Moose Paksoy <paksoy@gmail.com>
2017-07-18 07:34:50 -0700

    wip

--- routes/routes.js
+++ routes/routes.js
@@ -10,6 +10,10 @@ router.get('/', function(req, res, next) {
   res.render('home');
 });
 
+router.get('/moose', function(req, res) {
+  res.send('You have reached the Moose route');
+});
+
 ///////////////////////////// END OF PUBLIC ROUTES /////////////////////////////
 
 router.use(function(req, res, next){
