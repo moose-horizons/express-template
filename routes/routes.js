2255b55df4b7ac97dc9f0370536678930b9076e7
Moose Paksoy <mustafa@joinhorizons.com>
2017-07-18 07:53:57 -0700

    Delete routes again

--- routes/routes.js
+++ routes/routes.js
@@ -10,14 +10,6 @@ router.get('/', function(req, res, next) {
   res.render('home');
 });
 
-router.get('/moose', function(req, res) {
-  res.send('You have reached the Moose route');
-});
-
-router.get('/ricky', function(req, res, next) {
-  res.send('Ricky route!');
-});
-
 ///////////////////////////// END OF PUBLIC ROUTES /////////////////////////////
 
 router.use(function(req, res, next){
